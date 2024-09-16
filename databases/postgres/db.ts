443d7428a1f06b4905299029878024eac5093d4f
Vaggelis Magonezos <nospipian@gmail.com>
2024-09-16 11:13:02 +0300

    Update db.ts

--- databases/postgres/db.ts
+++ databases/postgres/db.ts
@@ -5,41 +5,41 @@ const prismaClientSingleton = () => {
   return new PrismaClient().$extends({
     query: {
       transaction: {
-        async create({ args, query }) {
-          const updatedTransaction = (await query(args)) as any
-          await updateWallet("save", updatedTransaction)
+        async create({ args, query }: any) {
+          const updatedTransaction = (await query(args)) as any;
+          await updateWallet("save", updatedTransaction);
 
-          return updatedTransaction
+          return updatedTransaction;
         },
 
-        async update({ args, query }) {
+        async update({ args, query }: any) {
           const prevTransaction = await prisma.transaction.findUnique({
             where: { id: args.where.id },
-          })
+          });
 
-          if (!prevTransaction) throw new Error("Transaction not found")
-          const updatedTransaction = (await query(args)) as any
-          await updateWallet("update", prevTransaction, updatedTransaction)
+          if (!prevTransaction) throw new Error("Transaction not found");
+          const updatedTransaction = (await query(args)) as any;
+          await updateWallet("update", prevTransaction, updatedTransaction);
 
-          return updatedTransaction
+          return updatedTransaction;
         },
 
-        async delete({ args, query }) {
+        async delete({ args, query }: any) {
           const transactionToDelete = await prisma.transaction.findUnique({
             where: { id: args.where.id },
-          })
+          });
 
-          if (!transactionToDelete) throw new Error("Transaction not found")
+          if (!transactionToDelete) throw new Error("Transaction not found");
 
-          const result = await query(args)
+          const result = await query(args);
 
-          await updateWallet("delete", transactionToDelete)
+          await updateWallet("delete", transactionToDelete);
 
-          return result
+          return result;
         },
       },
     },
-  })
+  });
 }
 
 declare const globalThis: {
