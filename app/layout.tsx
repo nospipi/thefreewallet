b4df830b9d7a3fb59b9a17cd7fdd77b7e4f2318d
Vaggelis Magonezos <nospipian@gmail.com>
2024-12-18 13:50:19 +0200

    added vercel analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import type { Metadata } from "next"
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { auth } from "@/auth"
-import SessionProvider from "./SessionProvider.client"
-import NavMenu from "./NavMenu.client"
-import { Toaster } from "react-hot-toast"
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { auth } from "@/auth";
+import SessionProvider from "./SessionProvider.client";
+import NavMenu from "./NavMenu.client";
+import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "TheFreeWallet",
   description: "A simple and free budgeting tool",
-}
+};
 
 //---------------------------------------------------------
 
 const RootLayout = async ({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) => {
-  const session = await auth()
+  const session = await auth();
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
@@ -30,13 +31,14 @@ const RootLayout = async ({
             {session && <NavMenu />}
             <main className="flex flex-1 h-full overflow-hidden bg-theme-google p-2">
               {children}
+              <Analytics />
             </main>
             <Toaster position="bottom-center" />
           </SessionProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
